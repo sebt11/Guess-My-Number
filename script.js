088f775ab6e9c92925e4aceabd913de34fc67987
sebt11 <sawentbwetha11@gmail.com>
2024-03-06 12:31:29 +0630

    First Commit

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() *20) +1;
+let score = 20;
+
+
+
+document.querySelector('.check').addEventListener('click',function(){
+const guess = Number (document.querySelector('.guess').value);
+console.log(guess, typeof guess);
+//when there is no input
+if(!guess){
+    document.querySelector('.message').textContent = '⛔No Number!';
+
+    //when player wins
+} else if(guess === secretNumber ){
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //when guess is too high
+}else if (guess > secretNumber){
+    if(score>1){
+    document.querySelector('.message').textContent = '📈Too high!';
+    score --;
+    document.querySelector('.score').textContent = score;
+    }else{
+        document.querySelector('.message').textContent = '💥You lost the game!'
+    }
+    //when guess is too low
+}else if(guess < secretNumber){
+    if(score>1){
+        document.querySelector('.message').textContent = '📉Too low!';
+        score --;
+        document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = 'You lost the game!'
+        }
+}
+});
+
+document.querySelector('.again').addEventListener('click',function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20)+1;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent = 'Start Guessing...';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+
+    document.querySelector('.number').style.width = '15rem';
+
+    
+})
+;
